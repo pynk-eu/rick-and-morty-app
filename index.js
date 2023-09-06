632587ec44b3e063b61314510e8d6773a11cfbb9
reverbePrintemps <59572064+reverbePrintemps@users.noreply.github.com>
2023-09-06 14:57:37 +0200

    Create createCharacterCard function and use it

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+import { createCharacterCard } from "./components/card/card.js";
+
 const cardContainer = document.querySelector('[data-js="card-container"]');
 const searchBarContainer = document.querySelector(
   '[data-js="search-bar-container"]'
@@ -9,6 +11,22 @@ const nextButton = document.querySelector('[data-js="button-next"]');
 const pagination = document.querySelector('[data-js="pagination"]');
 
 // States
-const maxPage = 1;
-const page = 1;
-const searchQuery = "";
+let maxPage = 1;
+let page = 1;
+let searchQuery = "";
+
+// Fetch data
+const fetchCharacters = async () => {
+  try {
+    const characters = await fetch(`https://rickandmortyapi.com/api/character`);
+    const charactersData = await characters.json();
+    const characterCards = await charactersData.results.map((character) =>
+      createCharacterCard(character)
+    );
+    cardContainer.innerHTML = characterCards;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+fetchCharacters();
